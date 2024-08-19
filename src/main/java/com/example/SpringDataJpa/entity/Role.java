9cc0a04b34582b7f68c3b33c36f8dc1b004d5916
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-08-19 11:52:51 +0330

    Create User and Role Entities

---
+++ src/main/java/com/example/SpringDataJpa/entity/Role.java
@@ -0,0 +1,23 @@
+package com.example.SpringDataJpa.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
---
+++ src/main/java/com/example/SpringDataJpa/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.SpringDataJpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users",
+    uniqueConstraints =
+            @UniqueConstraint(name = "unique_email",columnNames = "email")
+)
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
