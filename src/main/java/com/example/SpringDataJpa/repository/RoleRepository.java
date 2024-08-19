0aa75294542b58d4efafd8ca80aff452f05acbbc
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-08-19 11:55:53 +0330

    Create UserRepository and RoleRepository

---
+++ src/main/java/com/example/SpringDataJpa/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.SpringDataJpa.repository;
+
+import com.example.SpringDataJpa.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
