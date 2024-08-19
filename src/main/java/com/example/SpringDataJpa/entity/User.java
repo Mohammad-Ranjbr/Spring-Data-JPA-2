59f988f1aef023e9972cbec026d1f1685a977069
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2024-08-19 12:05:01 +0330

    Establish Unidirectional Many-To-Many Mapping Between User and Role

--- src/main/java/com/example/SpringDataJpa/entity/User.java
+++ src/main/java/com/example/SpringDataJpa/entity/User.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
 @Entity
@@ -24,5 +26,11 @@ public class User {
     private String lastName;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+                joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
 
 }
